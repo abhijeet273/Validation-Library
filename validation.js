42bea3f0860659f82d02e788adf448d2b73824a5
abhijeet273 <40641869+abhijeet273@users.noreply.github.com>
2019-04-23 12:01:49 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,121 +1,121 @@
 (function () {
 'use strict';
 
-// selecting all the input elements///
-	var inputs = document.getElementsByTagName('input');
-	var textareas = document.getElementsByTagName('textarea');
-	var selects = document.getElementsByTagName('select');
+// selecting all the input elements
+  var inputs = document.getElementsByTagName('input');
+  var textareas = document.getElementsByTagName('textarea');
+  var selects = document.getElementsByTagName('select');
 
 // defining a function validation 
-	function  validation(params){
-		for(i=0; i<params.length; i++){
+  function  validation(params){
+    for(i=0; i<params.length; i++){
 
-			if(params[i].type === 'required'){
-			  if (this.value.trim()===''){
-				  err.push('cannot be empty');
-			  }
-			}
+      if(params[i].type === 'required'){
+        if (this.value.trim()===''){
+          err.push('cannot be empty');
+        }
+      }
 
-			else if(params[i].type === 'regex') {
-			  if(!this.value.match(params[i].pattern)) {
-			  	err.push('Invalid Format');
-			  }
-			}
+      else if(params[i].type === 'regex') {
+        if(!this.value.match(params[i].pattern)) {
+          err.push('Invalid Format');
+        }
+      }
 
-			else if(params[i].type === 'length') {
-			  if(!isNaN(params[i].min) && this.value.length < params[i].min) {
-					err.push('Minimum '+params[i].min+' characters required');
-			  }
+      else if(params[i].type === 'length') {
+        if(!isNaN(params[i].min) && this.value.length < params[i].min) {
+          err.push('Minimum '+params[i].min+' characters required');
+        }
 
-			  if(!isNaN(params[i].max) && this.value.length > params[i].max) {
-					err.push('Maximum characters limit of '+params[i].max+' exceeded');
-			  }
+        if(!isNaN(params[i].max) && this.value.length > params[i].max) {
+          err.push('Maximum characters limit of '+params[i].max+' exceeded');
+        }
 
-			  if(!isNaN(params[i].just) && this.value.length !== params[i].just) {
-					err.push('It should be '+params[i].just+' characters long');
-			  }
-			}
+        if(!isNaN(params[i].just) && this.value.length !== params[i].just) {
+          err.push('It should be '+params[i].just+' characters long');
+        }
+      }
 
-			else if(params[i].type === 'different') {
-			  if(this.value === params[i].from) {
-				err.push('It should be different from your Email');
-			  }
-			}
+      else if(params[i].type === 'different') {
+        if(this.value === params[i].from) {
+        err.push('It should be different from your Email');
+        }
+      }
 
-			else if(params[i].type === 'member') {
-			  var count = 0;
-			  for(j=0; j < params[i].of.length; j++) {
-					if(this.value === params[i].of[j]){
-				  	count = count +1;
-				}
-			  }
+      else if(params[i].type === 'member') {
+        var count = 0;
+        for(j=0; j < params[i].of.length; j++) {
+          if(this.value === params[i].of[j]){
+            count = count +1;
+        }
+        }
 
-			  if(count === 0) {
-					err.push('It is not a member');
-			  }
-			}
+        if(count === 0) {
+          err.push('It is not a member');
+        }
+      }
 
-			else if(params[i].type === 'date') {
-			  var date = new Date(this.value);
-			  if(date.valueOf() < params[i].after) {
-					var mindate = new Date(params[i].after);
-					err.push('Please select a date after ' + mindate.toLocaleDateString());
-			  }
+      else if(params[i].type === 'date') {
+        var date = new Date(this.value);
+        if(date.valueOf() < params[i].after) {
+          var mindate = new Date(params[i].after);
+          err.push('Please select a date after ' + mindate.toLocaleDateString());
+        }
 
-			  if(date.valueOf()>params[i].before) {
-					var maxdate = new Date(params[i].before);
-					err.push('please select a date before '+ maxdate.toLocaleDateString());
-			  }
-			}
+        if(date.valueOf()>params[i].before) {
+          var maxdate = new Date(params[i].before);
+          err.push('please select a date before '+ maxdate.toLocaleDateString());
+        }
+      }
 
-			else if(params[i].type === 'file') {
-			  function hasExtension(name, exts) {
-					return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(name);
-			  }
+      else if(params[i].type === 'file') {
+        function hasExtension(name, exts) {
+          return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(name);
+        }
 
-			  if (!hasExtension(this.value, params[i].ext)) {
-					err.push('Invalid file!!');
-			  }
+        if (!hasExtension(this.value, params[i].ext)) {
+          err.push('Invalid file!!');
+        }
 
-			  if(this.files[0].size > params[i].max) {
-					err.push('file size should not exceed : ' + params[i].max);
-			  }
-			}
+        if(this.files[0].size > params[i].max) {
+          err.push('file size should not exceed : ' + params[i].max);
+        }
+      }
 
-			else if(params[i].type === 'email') {
-			  if(!this.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-					err.push('Please enter correct email (e.g. something@example.com)');
-			  }
-			}
+      else if(params[i].type === 'email') {
+        if(!this.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+          err.push('Please enter correct email (e.g. something@example.com)');
+        }
+      }
 
 
-			}
+      }
 
-			if(err.length === 0) {
-				return true;
-			}
+      if(err.length === 0) {
+        return true;
+      }
 
-			else {
-				return err;
-			}
-		  }
+      else {
+        return err;
+      }
+      }
 
 
 // defining function validate for each input Object
   for(var i=0; i<inputs.length; i++) {
     var err = [];
-		inputs[i].validate = validation
+    inputs[i].validate = validation
   }
 
 //  setting the validate function to the prototype of textarea and select 
   for(var i=0; i<textareas.length; i++) {
     var err = [];
-		textareas[i].validate = validation
+    textareas[i].validate = validation
   }
 
   for(var i=0; i<selects.length; i++) {
     var err = [];
-		selects[i].validate = validation
+    selects[i].validate = validation
   }
 
 })();
